26f069e4cf99dc0e79129d9da508558f007546ec
paulvscode <67148083+paulvscode@users.noreply.github.com>
2022-04-25 17:59:14 +0200

    bdd clean

--- migrations/Version20220425155813.php
+++ migrations/Version20220425155813.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220425144216 extends AbstractMigration
+final class Version20220425155813 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,6 @@ final class Version20220425144216 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE article_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE article_category_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE event_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE event_category_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE project_category_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE test_vich_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE social_event_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE team_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE project (id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, long_description TEXT NOT NULL, image VARCHAR(255) NOT NULL, link VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN project.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE social_event (id INT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, link VARCHAR(255) DEFAULT NULL, icon VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
@@ -38,14 +30,6 @@ final class Version20220425144216 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE social_event_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE team_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE article_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE article_category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE event_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE event_category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE project_category_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE test_vich_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE project');
         $this->addSql('DROP TABLE social_event');
         $this->addSql('DROP TABLE team');
