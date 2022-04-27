65d8c2e39491051485ae6eba3aacf0cdab1a421e
paulvscode <67148083+paulvscode@users.noreply.github.com>
2022-04-27 20:45:56 +0200

    added dynamic socialEvent with counter

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Entity\SocialEvent;
+use App\Entity\Team;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,8 +19,18 @@ class HomePageController extends AbstractController
 //    }
 
     #[Route('/', name: 'homepage_')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('base.html.twig', []);
+        $colorsClass = ['blue', 'orange', 'green', 'red', 'purple', 'pink'];
+        $socialEvents = $doctrine->getRepository(SocialEvent::class)->findAll();
+        $teamMembers = $doctrine->getRepository(Team::class)->findAll();
+        $projects = $doctrine->getRepository(Project::class)->findAll();
+
+        return $this->render('base.html.twig', [
+            'teamMembers' => $teamMembers,
+            'socialEvents' => $socialEvents,
+            'colors' => $colorsClass,
+            'projects' => $projects
+        ]);
     }
 }
