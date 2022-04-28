7669775f0a87dcb265145a74a130a91be18cef4b
paulvscode <67148083+paulvscode@users.noreply.github.com>
2022-04-28 22:09:32 +0200

    fields configuration easy admin

--- src/Controller/Admin/ProjectCrudController.php
+++ src/Controller/Admin/ProjectCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Project;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProjectCrudController extends AbstractCrudController
 {
@@ -12,14 +16,16 @@ class ProjectCrudController extends AbstractCrudController
         return Project::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
+            TextEditorField::new('longDescription'),
+            TextField::new('image'),
+            TextField::new('link'),
+            DateTimeField::new('createdAt'),
+
         ];
     }
-    */
 }
