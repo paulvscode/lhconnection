58f12a2d290e88516329cc130428c1bb7aeaeb02
paulvscode <67148083+paulvscode@users.noreply.github.com>
2022-05-24 10:29:50 +0200

    l

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -6,8 +6,12 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class RequestListener
 {
+    public function __construct() {
+        dd('toto');
+    }
+
     public function onKernelRequest(RequestEvent $event)
     {
-
+        dd('titi');
     }
 }
