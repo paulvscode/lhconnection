5c11d41cbf17546e430b83a138964e530e14c290
paulvscode <67148083+paulvscode@users.noreply.github.com>
2022-06-26 13:25:11 +0200

    added members

--- src/Controller/User/UserDashboardController.php
+++ src/Controller/User/UserDashboardController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller\User;
 
+use App\Entity\Team;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserDashboardController extends AbstractController
 {
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
         $user = $this->getUser();
         $username = $user->getUsername();
+        $teamMembers = $doctrine->getRepository(Team::class)->findAll();
+
 
         return $this->render('user/user_dashboard/index.html.twig', [
-            'username' => $username
+            'username' => $username,
+            'teamMembers' => $teamMembers
         ]);
     }
 }
